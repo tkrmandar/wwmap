e9ff4e7055aabd4730c387096ab80bcc259d65d3
tkrmandar <98222746+tkrmandar@users.noreply.github.com>
2022-11-01 10:35:06 +0530

    add files via upload

---
+++ questions.js
@@ -0,0 +1,71 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "India is a federal union comprising twenty-eight states and how many union territories?",
+    answer: "8",
+    options: [
+      "7",
+      "6",
+      "10",
+      "8"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Currently, how many countries are located in Africa?",
+    answer: "54",
+    options: [
+      "50",
+      "54",
+      "59",
+      "61"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is the largest landlocked country in the world?",
+    answer: "Kazakhstan",
+    options: [
+      "mongolia",
+      "Niger",
+      "Kazakhstan",
+      "Ethiopia"
+    ]
+  },
+    {
+    numb: 4,
+    question: "which county is also known as (land of rising sun)?",
+    answer: "Japan",
+    options: [
+      "New zealand",
+      "Japan",
+      "Fiji",
+      "China",
+    ]
+  },
+    {
+    numb: 5,
+    question: "which continent has the highest number of countries?",
+    answer: "Africa",
+    options: [
+      "Africa",
+      "asia",
+      "europe",
+      "north america"
+    ]
+  },
+  
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
